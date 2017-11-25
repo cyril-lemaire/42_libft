a9764e11436dd7a84dada253ce8e62b99eca3894
Cyril LEMAIRE <clemaire@student.42.fr>
2017-11-25 10:05:39 +0100

    Don't forget the .h baka!

--- libft.h
+++ libft.h
@@ -6,23 +6,40 @@
 /*   By: clemaire <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/17 12:46:42 by clemaire          #+#    #+#             */
-/*   Updated: 2017/11/17 16:38:52 by clemaire         ###   ########.fr       */
+/*   Updated: 2017/11/24 17:37:50 by clemaire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-
-#define
+# include <string.h>
 
 char		*ft_strcpy(char *dst, char *src);
+char		*ft_strncpy(char *dst, char *src, size_t len);
 int			ft_strcmp(char *s1, char *s2);
+int			ft_strncmp(char *s1, char *s2, size_t n);
 size_t		ft_strlen(const char *s);
 char		*ft_strdup(char *s);
 void		ft_bzero(void *s, size_t n);
-void		*memchr(const void *s, int c, size_t n);
+void		*ft_memchr(const void *s, int c, size_t n);
 void		*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void		*ft_memcpy(void *dst, const void *src, size_t n);
-void		*memmove(void *dst, const void *src, size_t len);
+void		*ft_memmove(void *dst, const void *src, size_t len);
+void		*ft_memset(void *b, int c, size_t len);
+char		*ft_strcat(char *s1, const char *s2);
+char		*ft_strchr(const char *s, int c);
+size_t		ft_strlcat(char *dst, const char *src, size_t size);
+char		*ft_strncat(char *restrict s1, const char *restrict s2, size_t n);
+char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
+char		*ft_strrchr(const char *s, int c);
+char		*ft_strstr(const char *haystack, const char *needle);
+int			ft_atoi(const char *str);
+int			ft_isascii(int c);
+int			ft_isdigit(int c);
+int			ft_isalpha(int c);
+int			ft_isprint(int c);
+int			ft_isalnum(int c);
+int			ft_tolower(int c);
+int			ft_toupper(int c);
 
 #endif
