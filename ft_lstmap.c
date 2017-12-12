fe9da0dcf36b4ba02ff96d468e7b14b305304a8e
Cyril LEMAIRE <clemaire@student.42.fr>
2017-12-12 11:50:21 +0100

    Corrected norm issue (added missing header file)

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: clemaire <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/29 14:00:01 by clemaire          #+#    #+#             */
-/*   Updated: 2017/12/04 15:26:32 by clemaire         ###   ########.fr       */
+/*   Updated: 2017/12/12 11:44:02 by clemaire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <string.h>
 #include "libft.h"
 
-static void	ft_del_content(void *content, size_t content_size)
+static void		ft_del_content(void *content, size_t content_size)
 {
 	(void)content_size;
 	free(content);
@@ -35,8 +35,9 @@ t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 	current_cell = new_lst;
 	while (lst != NULL)
 	{
-		if ((f_ret = f(lst)) == NULL || (current_cell->next =\
-			ft_lstnew(f_ret->content, f_ret->content_size)) == NULL)
+		if ((f_ret = f(lst)) == NULL ||
+			(current_cell->next = ft_lstnew(f_ret->content,
+											f_ret->content_size)) == NULL)
 		{
 			ft_lstdel((void*)new_lst, ft_del_content);
 			return (NULL);
