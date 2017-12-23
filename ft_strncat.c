b45746d19e228f178e272ae1d8803d0e6fdb2c03
Cyril LEMAIRE <clemaire@student.42.fr>
2017-12-23 08:54:24 +0100

    Fixed strncat (Now find why these are so slow, and how the f* can the strstr be quick when they rely on these?)

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: clemaire <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 10:58:08 by clemaire          #+#    #+#             */
-/*   Updated: 2017/11/29 10:47:24 by clemaire         ###   ########.fr       */
+/*   Updated: 2017/12/23 08:29:26 by clemaire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strncat(char *s1, const char *s2, size_t n)
 	i = 0;
 	while (s1[i] != '\0')
 		++i;
-	while (*s2 != '\0' && i < n)
+	while (*s2 != '\0' && n--)
 		s1[i++] = *(s2++);
 	s1[i] = '\0';
 	return (s1);
