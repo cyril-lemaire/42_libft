3c11620f79c2db5f9a98efc34b72c25a24f1a0f7
Cyril LEMAIRE <clemaire@student.42.fr>
2018-04-21 04:48:30 +0200

    Corrected stradd behaviour (no longer appends to dst, if I do that function it'll be stracat)

--- ft_stradd.c
+++ ft_stradd.c
@@ -1,13 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stradd.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clemaire <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/21 04:38:09 by clemaire          #+#    #+#             */
+/*   Updated: 2018/04/21 04:42:26 by clemaire         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 #include <string.h>
+#include <stdlib.h>
 
-char	*ft_stradd(char **dest, const char *src)
+char	*ft_stradd(const char *s1, const char *s2)
 {
-	const int		dst_len = ft_strlen(*dst);
-	const int		src_len = ft_strlen(src);
+	const size_t	s1_len = ft_strlen(s1);
+	const size_t	s2_len = ft_strlen(s2);
+	char			*res;
 
-	if ((*dst = ft_realloc(*dst, dst_len + src_len + 1)) == NULL)
+	if ((res = (char*)malloc(s1_len + s2_len + 1)) == NULL)
 		return (NULL);
-	ft_memcpy(&(*dst[dst_len]), src, src_len + 1);
-	return (src_len);
+	ft_memcpy(res, s1, s1_len);
+	return (ft_memcpy(res, s2, s2_len + 1));
 }
